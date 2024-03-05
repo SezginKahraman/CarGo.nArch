ab92c2f797ed83936c87d4ca70754d4281c6339c
SezginKahraman <sezgin.kahraman12@gmail.com>
2024-03-06 00:41:16 +0300

    Add project files.

---
+++ Cargo.Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace CarGo.Application.Features.Brands.Commands.Create
+{                                              // what this request is going to response as. 
+    public class CreateBrandCommand : IRequest<CreatedBrandResponse> // it says, you are an request which is going to be used by user (The request will come from API !) 
+    {
+        // A createCommand will come through as a request, this request will be mapped to domain, then saved to the database.
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+        {
+            public Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+
+
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
