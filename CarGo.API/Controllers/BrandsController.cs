678432d87b567e482988097349c0274acfad10a2
SezginKahraman <sezgin.kahraman12@gmail.com>
2024-03-08 18:09:18 +0300

    Query handler is added.

--- CarGo.API/Controllers/BrandsController.cs
+++ CarGo.API/Controllers/BrandsController.cs
@@ -1,4 +1,8 @@
 ﻿using CarGo.Application.Features.Brands.Commands.Create;
+using CarGo.Application.Features.Brands.Queries.GetById;
+using CarGo.Application.Features.Brands.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Responses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +16,24 @@ namespace CarGo.API.Controllers
         public async Task<IActionResult> Add([FromBody]CreateBrandCommand createBrandCommand)
         {
             CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery query = new GetListBrandQuery(){PageRequest = pageRequest};
+            GetListResponse<GetListBrandListItemDto> response = await Mediator.Send(query);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            GetByIdBrandQuery query = new GetByIdBrandQuery() { Id = id };
+            GetByIdBrandResponse response = await Mediator.Send(query);
+
             return Ok(response);
         }
     }
--- Cargo.Application/Features/Brands/Profiles/MappingProfiles.cs
+++ Cargo.Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -5,7 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using CarGo.Application.Features.Brands.Commands.Create;
+using CarGo.Application.Features.Brands.Queries.GetById;
+using CarGo.Application.Features.Brands.Queries.GetList;
 using CarGo.Domain.Entities;
+using Core.Application.Responses;
+using Core.Persistance.Paging;
 
 namespace CarGo.Application.Features.Brands.Profiles
 {
@@ -15,6 +19,9 @@ namespace CarGo.Application.Features.Brands.Profiles
         {
             CreateMap<Brand, CreateBrandCommand>().ReverseMap();
             CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+            CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
+            CreateMap<Brand, GetByIdBrandResponse>().ReverseMap();
+            CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap();
         }
     }
 }
